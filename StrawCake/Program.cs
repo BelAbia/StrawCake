8ce651b30403a26c3837c4ea71b6ec1e305b8887
unknown <isabela.castro@inventsoftware.com.br>
2024-08-22 13:54:39 -0300

    Configurado hangfire e RavenDB no projeto.

--- StrawCake/Program.cs
+++ StrawCake/Program.cs
@@ -1,4 +1,7 @@
+using Hangfire;
+using Hangfire.Raven.Storage;
 using StrawCake.Dominio.Servicos;
+using System.Security.Cryptography.X509Certificates;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +13,34 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ServicoBolo>();
 
+var conexaoDoHangfire = Environment.GetEnvironmentVariable("URL_CLIENT_PESSOAL")
+        ?? throw new Exception($"Variável de Ambiente [URL_CLIENT_PESSOAL] não configurada, por favor informar a conexão com o banco!");
+
+var certificado = new X509Certificate2(Environment.GetEnvironmentVariable("CERTIFICADO_PESSOAL"));
+
+builder.Services.AddHangfire(op =>
+{
+    conexaoDoHangfire
+    .Split(',')
+    .ToList()
+    .ForEach(url =>
+    {
+        if (certificado != null)
+        {
+            op.UseRavenStorage(url, "HangfireDB", certificado);
+        }
+        else
+        {
+            op.UseRavenStorage(url, "HangfireDB");
+        }
+    });
+});
+builder.Services.AddHangfireServer();
+
+
+
 var app = builder.Build();
+app.UseHangfireDashboard();
 
 app.UseHttpsRedirection();
 
@@ -19,3 +49,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+var client = new BackgroundJobServer();
